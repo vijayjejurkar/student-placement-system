4bdbd2f47b5c717bfd79f2109802b4ea6b8ba8d0
vijayjejurkar <38349106+vijayjejurkar@users.noreply.github.com>
2021-02-10 14:43:10 +0530

    Update StudentPlacementSystemApplication.java
    Updated code to run the pipeline

--- src/main/java/com/app/sps/StudentPlacementSystemApplication.java
+++ src/main/java/com/app/sps/StudentPlacementSystemApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentPlacementSystemApplication {
+public class StudentPlacementSystemApplication 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentPlacementSystemApplication.class, args);
